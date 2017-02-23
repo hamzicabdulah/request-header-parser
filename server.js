a69492f2a5f58db62598dfae31262a9cb87af232
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-02-23 17:57:58 +0000

    Fully functional server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/api/whoami', function (req, res) {
+    var language = req.headers['accept-language'],
+    agent = req.headers['user-agent'];
+    
+    var object = {
+        ipaddress: req.headers['x-forwarded-for'],
+        language: language.slice(0, language.indexOf(',')),
+        software: agent.slice(agent.indexOf('(')+1, agent.indexOf(')'))
+    };
+    
+    res.send(object);
 });
 
 app.listen(process.env.PORT || 8080, function() {
